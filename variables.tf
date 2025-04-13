494c354bb4613e10f817cdbb7eee387b42aaa6b3
vishnureddy <tarigopula34@gmail.com>
2025-04-13 20:37:22 +0530

    vault

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "tools" {
+  default = {
+
+  }
+  vault = {
+    port        = 8200
+    volume_size = 20
+    instance_type = "t3.small"
+
+  }
+}
+variable "zone_id" {
+  default = "Z02665523CCE7X5KKVOH8"
+}
+
+variable "domain_name" {
+  default = "vishnuredddy.online"
+}
